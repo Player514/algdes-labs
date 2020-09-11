f136a54e8eadd70346e79546cd176729ff10a25b
Anders Fischer-Nielsen <andersfischern@me.com>
2020-09-11 16:29:28 +0200

    Add more iterations to head-to-head tests

--- closest-points/head-to-head.sh
+++ closest-points/head-to-head.sh
@@ -1,23 +1,73 @@
 #!/bin/sh
-printf "KD-tree:             "
-start=$SECONDS
-for FILE_NAME in data/*-tsp.txt; do
-	python3 src/closestPoints.py < $FILE_NAME > /dev/null
-done
-
-for FILE_NAME in data/*-tsp.txt; do
-	python3 src/closestPoints.py < $FILE_NAME > /dev/null
-done
-
-for FILE_NAME in data/*-tsp.txt; do
-	python3 src/closestPoints.py < $FILE_NAME > /dev/null
-done
-duration=$(( SECONDS - start ))
-echo $duration
-
+echo "1:"
 printf "Divide-and-conquer:  "
 start=$SECONDS
 for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+duration=$(( SECONDS - start ))
+echo $duration
+
+printf "KD-tree:             "
+start=$SECONDS
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+duration=$(( SECONDS - start ))
+echo $duration
+
+echo "7:"
+printf "Divide-and-conquer:  "
+start=$SECONDS
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+duration=$(( SECONDS - start ))
+echo $duration
+
+printf "KD-tree:             "
+start=$SECONDS
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
     python3 src/kd-solver.py < $FILE_NAME > /dev/null
 done
 
@@ -31,10 +81,171 @@ done
 duration=$(( SECONDS - start ))
 echo $duration
 
-# printf "Divide-and-conquer 2:"
-# start=$SECONDS
-# for FILE_NAME in data/*-tsp.txt; do
-#     python3 src/Andreascp.py < $FILE_NAME > /dev/null
-# done
-# duration=$(( SECONDS - start ))
-# echo $duration
+echo "20:"
+printf "Divide-and-conquer:  "
+start=$SECONDS
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+	python3 src/closestPoints.py < $FILE_NAME > /dev/null
+done
+duration=$(( SECONDS - start ))
+echo $duration
+
+printf "KD-tree:             "
+start=$SECONDS
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+
+for FILE_NAME in data/*-tsp.txt; do
+    python3 src/kd-solver.py < $FILE_NAME > /dev/null
+done
+duration=$(( SECONDS - start ))
+echo $duration
