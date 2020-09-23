8f7ab07a2bac5d0cfff6c409bfeef02b800fe63d
Anders Fischer-Nielsen <andersfischern@me.com>
2020-09-23 16:18:55 +0200

    Improve parsing

--- gorilla/solution.py
+++ gorilla/solution.py
@@ -24,22 +24,20 @@ def print_data(data):
     for t in data: 
         print(f"{t[0]:>15}, {t[1]}")
 
-char_index = { 'A': 0, 'R': 1, 'N': 2, 'D': 3, 'C': 4, 'Q': 5, 'E': 6, 'G': 7, \
-               'H': 8, 'I': 9, 'L':10, 'K':11, 'M':12, 'F':13, 'P':14, 'S':15, \
-               'T':16, 'W':17, 'Y':18, 'V':19, 'B':20, 'Z':21, 'X':22, '*':23 }
-def penalty(f, t="*", blosum=[]):    
-    first_i = char_index[f]
-    second_i = char_index[t]
-    return blosum[first_i][second_i]
+def penalty(f, t="*", blosum=[], indices={}):    
+    return blosum[indices[f]][indices[t]]
 
 def parse_blosum(location="data/BLOSUM62.txt"): 
     regex = re.compile("(^\w |\* )")
-    blosum = open(location, "r").readlines()[7:]
+    all_lines = open(location, "r").readlines()
+    indices = all_lines[6].strip().split('  ')
+    as_dict = {v: i for i, v in enumerate(indices)}
+    blosum = all_lines[7:]
     blosum = list(map(lambda line: list(map(lambda lll: int(lll), filter(lambda ll: ll != '', line.strip()[regex.search(line).lastindex+1:].split(' ')))), blosum))
-    return blosum
+    return blosum, as_dict
 
-def find_difference(i, j, x, y, cache, blosum, gap):
-
+def find_difference(i, j, x, y, cache, blosum, indices, gap):
+    
     #TODO: This never terminates... Why? 
 
     if i == 0: 
@@ -47,16 +45,16 @@ def find_difference(i, j, x, y, cache, blosum, gap):
     if j == 0: 
         return gap # What is the delta value here? 
 
-    mismatch_penalty = cache[i][j] if cache[i][j] != None else penalty(x[i], y[j], blosum)
+    mismatch_penalty = cache[i][j] if cache[i][j] != None else penalty(x[i], y[j], blosum, indices)
     cache[i][j] = mismatch_penalty
-    case_1 = mismatch_penalty + find_difference(i-1, j-1, x, y, cache, blosum, gap)
-    case_2 = gap + find_difference(i-1, j, x, y, cache, blosum, gap)
-    case_3 = gap + find_difference(i, j-1, x, y, cache, blosum, gap)
+    case_1 = mismatch_penalty + find_difference(i-1, j-1, x, y, cache, blosum, indices, gap)
+    case_2 = gap + find_difference(i-1, j, x, y, cache, blosum, indices, gap)
+    case_3 = gap + find_difference(i, j-1, x, y, cache, blosum, indices, gap)
 
     minimum = min([case_1, case_2, case_3])
     return minimum
 
-def find_differences(input, blosum, gap):
+def find_differences(input, blosum, indices, gap):
     differences = {}
     cache = None
     for i in reversed(range(0, len(input))):
@@ -67,7 +65,7 @@ def find_differences(input, blosum, gap):
             second_string = second_string + ('*' * (len(first_string)-len(second_string))) if len(first_string) > len(second_string) else second_string
 
             cache = [[None] * len(first_string)] * len(second_string)
-            difference = find_difference(len(first_string)-1, len(second_string)-1, first_string, second_string, cache, blosum, gap)
+            difference = find_difference(len(first_string)-1, len(second_string)-1, first_string, second_string, cache, blosum, indices, gap)
             
             first_name = input[i][0]
             second_name = input[j][0]
@@ -83,6 +81,6 @@ def print_differences(differences):
 
 gap = -4
 data = parse_input(sys.stdin)
-blosum = parse_blosum("data/BLOSUM62.txt")
-differences = find_differences(data, blosum, gap)
+blosum, indices = parse_blosum("data/BLOSUM62.txt")
+differences = find_differences(data, blosum, indices, gap)
 print_differences(differences)
