0f169a22ee72bd211e67642550785f6c8f08a0b4
Anders Fischer-Nielsen <andersfischern@me.com>
2020-09-23 16:01:13 +0200

    Implement first attempt at solution (has termination issues)

--- gorilla/solution.py
+++ gorilla/solution.py
@@ -38,14 +38,51 @@ def parse_blosum(location="data/BLOSUM62.txt"):
     blosum = list(map(lambda line: list(map(lambda lll: int(lll), filter(lambda ll: ll != '', line.strip()[regex.search(line).lastindex+1:].split(' ')))), blosum))
     return blosum
 
-def find_differences(input):
-    for i in range(0, len(input)):
-        for j in range(i+1, len(input)):
-            first = input[i]
-            second = input[j]
+def find_difference(i, j, x, y, cache, blosum, gap):
 
-            print(first, '---', second)
+    #TODO: This never terminates... Why? 
 
+    if i == 0: 
+        return gap # What is the delta value here? 
+    if j == 0: 
+        return gap # What is the delta value here? 
+
+    mismatch_penalty = cache[i][j] if cache[i][j] != None else penalty(x[i], y[j], blosum)
+    cache[i][j] = mismatch_penalty
+    case_1 = mismatch_penalty + find_difference(i-1, j-1, x, y, cache, blosum, gap)
+    case_2 = gap + find_difference(i-1, j, x, y, cache, blosum, gap)
+    case_3 = gap + find_difference(i, j-1, x, y, cache, blosum, gap)
+
+    minimum = min([case_1, case_2, case_3])
+    return minimum
+
+def find_differences(input, blosum, gap):
+    differences = {}
+    cache = None
+    for i in reversed(range(0, len(input))):
+        for j in reversed(range(i+1, len(input))):
+            first_string = input[i][1]
+            second_string = input[j][1]
+            first_string = first_string + ('*' * (len(second_string)-len(first_string))) if len(second_string) > len(first_string) else first_string
+            second_string = second_string + ('*' * (len(first_string)-len(second_string))) if len(first_string) > len(second_string) else second_string
+
+            cache = [[None] * len(first_string)] * len(second_string)
+            difference = find_difference(len(first_string)-1, len(second_string)-1, first_string, second_string, cache, blosum, gap)
+            
+            first_name = input[i][0]
+            second_name = input[j][0]
+            combined = f"{first_name}--{second_name}"
+            resulting_string = "" # TODO: Implement finding actual difference string
+            differences[combined] = (difference, resulting_string)
+    
+    return differences
+
+def print_differences(differences):
+    for key, value in differences.items():
+        print(f"{key}: {value[0]}\n{value[1]}")
+
+gap = -4
 data = parse_input(sys.stdin)
 blosum = parse_blosum("data/BLOSUM62.txt")
-find_differences(data)
+differences = find_differences(data, blosum, gap)
+print_differences(differences)
